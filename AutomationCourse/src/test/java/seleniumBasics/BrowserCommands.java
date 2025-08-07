fd497069bc72009d372c6fa03be6c3cb19a5d24f
Lenovo <Lenovo@DESKTOP-NQ1KCMR>
2025-08-07 09:37:44 +0530

    Second Commit

---
+++ AutomationCourse/src/test/java/seleniumBasics/BrowserCommands.java
@@ -0,0 +1,30 @@
+package seleniumBasics;
+
+public class BrowserCommands extends Base {
+	public void handlingbrowsercommands()
+	{
+		String title=driver.getTitle();
+		System.out.println(title);
+		String url =driver.getCurrentUrl();
+		System.out.println(url);
+		String handleId=driver.getWindowHandle();
+		System.out.println(handleId);
+		String pageSource=driver.getPageSource();
+		System.out.println(pageSource);
+	}
+	 public void navigationCommands()
+	 {
+		 driver.navigate().to("https://www.amazon.in/");
+		 driver.navigate().back();
+		 driver.navigate().forward();
+		 driver.navigate().refresh();
+	 }
+	public static void main(String[] args)
+	{
+		BrowserCommands browser=new BrowserCommands();
+		browser.initializeBrowser();
+		//browser.handlingbrowsercommands();
+		browser.navigationCommands();
+	}
+
+}
